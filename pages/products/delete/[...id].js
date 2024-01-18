46d578b03b775549f34a705316c00012dfde9908
noklash <123834419+noklash@users.noreply.github.com>
2024-01-18 23:10:25 +0100

    delete product done

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -19,12 +19,29 @@ const deleteProductPage = () => {
     const goBack = () => {
         router.push('/products');
     }
+    const deleteProduct = async () => {
+        await axios.delete('/api/products?id='+id);
+        goBack();
+    }
 
   return (
    <Layout>
-    <h1>Do you really want to delete {productInfo?.title} ?</h1>
-    <button>Yes</button>
-    <button onClick={goBack}>No</button>
+    <h1 className='text-center'>Do you really want to delete &nbsp;"{productInfo?.title}"?</h1>
+    <div className='flex gap-2 justify-center'>
+        <button 
+            className='btn-red'
+            onClick={deleteProduct}>
+            Yes
+        </button>
+
+        <button 
+            className='btn-default' 
+            onClick={goBack}>
+            No
+        </button>
+    </div>
+   
+
    </Layout>
   )
 }
