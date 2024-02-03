68d80799cdeef3b033388863766eb7f35b245cb6
noklash <123834419+noklash@users.noreply.github.com>
2024-02-03 13:17:36 +0100

    removed middleware

--- components/Layout.js
+++ components/Layout.js
@@ -4,15 +4,15 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Layout({ children }) {
   const { data: session } = useSession()
 
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className='text-center w-full' >
-          <button onClick={() => signIn('google')} className="bg-white p-2 rounded-lg px-4">Login with Google</button>
-        </div>
-      </div>
-    )
-  }
+  // if(!session){
+  //   return (
+  //     <div className="bg-blue-900 w-screen h-screen flex items-center">
+  //       <div className='text-center w-full' >
+  //         <button onClick={() => signIn('google')} className="bg-white p-2 rounded-lg px-4">Login with Google</button>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div className="bg-blue-900 min-h-screen flex"> 
